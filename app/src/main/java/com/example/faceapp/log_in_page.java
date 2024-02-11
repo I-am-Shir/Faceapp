c9a0ffdcaaaf498962c150efc9c19113982c37de
I-am-Shir <ahrtskr@gmail.com>
2024-02-11 23:26:26 +0200

    tried fixing fragments

--- app/src/main/java/com/example/faceapp/log_in_page.java
+++ app/src/main/java/com/example/faceapp/log_in_page.java
@@ -9,39 +9,43 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class log_in_page extends AppCompatActivity {
-        private EditText user_name, pass_word;
-        private Button butLogin;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_sign_in);
+    private EditText user_name, pass_word;
+    private Button butLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
 
-            butLogin = findViewById(R.id.butLogin);
-            butLogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v){
-                    user_name = findViewById(R.id.username);
-                    pass_word = findViewById(R.id.password);
-                    String username = user_name.getText().toString();
-                    String password = pass_word.getText().toString();
+        butLogin = findViewById(R.id.butLogin);
+        butLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                user_name = findViewById(R.id.username);
+                pass_word = findViewById(R.id.password);
+                String username = user_name.getText().toString();
+                String password = pass_word.getText().toString();
 
-                    // Implement authentication logic here
-                    if (username.equals("Admin") && password.equals("123")) {
-                        // Successful login
-                        Toast.makeText(log_in_page.this, "Login successful", Toast.LENGTH_SHORT).show();
-                    } else {
-                        // Failed login
-                        Toast.makeText(log_in_page.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
-                        Intent i = new Intent(log_in_page.this, feed_page.class);
-                        startActivity(i);
-                    }
+                // Implement authentication logic here
+                if (username.equals("Admin") && password.equals("123")) {
+                    // Successful login
+                    Toast.makeText(log_in_page.this, "Login successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Failed login
+                    Toast.makeText(log_in_page.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(log_in_page.this, feed_page.class);
+                    startActivity(i);
                 }
+            }
+        });
 
-    //        Button butSignup = findViewById(R.id.butSignup);
-    //        butLogin.setOnClickListener(v -> {
-    //            Intent i = new Intent(log_in_page.this, sign_up_page.class);
-    //            startActivity(i);
-    //        });
-        }
+            Button butSignup = findViewById(R.id.butSignup);
+            butSignup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(log_in_page.this, sign_up_page.class);
+                    startActivity(i);
+                }
+            });
+
     }
 }
--- app/src/main/java/com/example/faceapp/sign_up_page.java
+++ app/src/main/java/com/example/faceapp/sign_up_page.java
@@ -1,5 +1,7 @@
 package com.example.faceapp;
 
+import static android.text.TextUtils.replace;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,13 +13,18 @@ public class sign_up_page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_page);
+        setContentView(R.layout.fragment_email1);
+
+        emailFragment1 email_fragment1 = new emailFragment1();
 
         backBut = findViewById(R.id.backBut);
         backBut.setOnClickListener(v -> {
             Intent i = new Intent(sign_up_page.this, log_in_page.class);
             startActivity(i);
         });
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragmentContainerView, emailFragment1.class, null).setReorderingAllowed(true)
+                .addToBackStack("name")
+            .commit();
+        }
     }
-
-}
