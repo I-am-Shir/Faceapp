e2703b44c7a7457277b3c9a4a1bc4dcfc32e0218
i-am-shir <94904806+I-am-Shir@users.noreply.github.com>
2024-02-14 22:39:26 +0200

    Working image capture

--- app/src/main/java/com/example/faceapp/PictureFrag4.java
+++ app/src/main/java/com/example/faceapp/PictureFrag4.java
@@ -3,11 +3,13 @@ package com.example.faceapp;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -17,30 +19,50 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.io.File;
 
 public class PictureFrag4 extends Fragment {
+
+    private static final int CAMERA_REQUEST = 1888;
     private Constraints constraints;
-    private Button photo_from_gallery, photo_from_camera, finishSignUpBut, backBut;;
+    private Button photo_from_gallery, photo_from_camera, finishSignUpBut, backBut;
+    ;
     private TextView galleryPicException, cameraPicException;
     private ImageView picturePreview;
     private Boolean picCheck;
-    private Uri imageUri;
-    ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
+    private Uri imageUri, uri;
+    private ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
+    private ActivityResultLauncher<Uri> mGetContent;
+
     public PictureFrag4() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Registers a photo picker activity launcher in single-select mode.
-       pickMedia = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
-                    // Callback is invoked after the user selects a media item or closes the
-                    // photo picker.
-                    if (uri != null) {
-                        Log.d("PhotoPicker", "Selected URI: " + uri);
-                        imageUri = uri;
-                    } else {
-                        Log.d("PhotoPicker", "No media selected");
+        pickMedia = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+            // Callback is invoked after the user selects a media item or closes the
+            // photo picker.
+            if (uri != null) {
+                Log.d("PhotoPicker", "Selected URI: " + uri);
+                imageUri = uri;
+            } else {
+                Log.d("PhotoPicker", "No media selected");
+            }
+        });
+
+        mGetContent = registerForActivityResult(
+                new ActivityResultContracts.TakePicture(),
+                new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        if (result) {
+                            imageUri = uri;
+                        } else {
+                            Log.d("PhotoPicker", "No photo taken");
+                        }
                     }
                 });
         // Inflate the layout for this fragment
@@ -62,7 +84,7 @@ public class PictureFrag4 extends Fragment {
         cameraPicException.setVisibility(View.GONE);
         finishSignUpBut.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 if (picCheck) {
                     // Sign_up_page sign_up_page = (Sign_up_page) getActivity();
                     // sign_up_page.replaceFragments(NameFrag3.class);
@@ -72,14 +94,14 @@ public class PictureFrag4 extends Fragment {
 
         backBut.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                 Sign_up_page sign_up_page = (Sign_up_page) getActivity();
-                 sign_up_page.replaceFragments(Log_in_page.class);
+            public void onClick(View v) {
+                Sign_up_page sign_up_page = (Sign_up_page) getActivity();
+                sign_up_page.replaceFragments(Log_in_page.class);
             }
         });
         photo_from_gallery.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 picturePreview = new ImageView(getContext());
                 picturePreview = getView().findViewById(R.id.picturePreview);
                 picturePreview.setImageURI(imageUri);
@@ -92,10 +114,14 @@ public class PictureFrag4 extends Fragment {
         });
         photo_from_camera.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                File file = new File(requireActivity().getFilesDir(), "picFromCamera");
+                uri = FileProvider.getUriForFile(requireActivity(), requireActivity().getPackageName() + ".provider", file);
+                mGetContent.launch(uri);
 
-
-
+                picturePreview = new ImageView(getContext());
+                picturePreview = getView().findViewById(R.id.picturePreview);
+                picturePreview.setImageURI(uri);
             }
         });
     }
